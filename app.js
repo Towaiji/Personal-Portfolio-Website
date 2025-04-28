0ae891e3a931bb0aa87829c8a939a2bd33219ce0
Ali Towaiji <145403626+Towaiji@users.noreply.github.com>
2025-04-28 02:56:01 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -93,3 +93,15 @@ function prevSlide() {
 }
 
 window.addEventListener("resize", updateCarousel);
+
+// LIGHTBOX CODE
+function openLightbox(src) {
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = document.getElementById("lightbox-img");
+    lightboxImg.src = src;
+    lightbox.style.display = "flex";
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
